9aee1cf82dcdc93288fff7356ec2956abaa2e755
prachiharwara <58542480+prachiharwara@users.noreply.github.com>
2022-02-01 10:39:35 +0530

    Add files via upload

---
+++ shellsort.py
@@ -0,0 +1,17 @@
+def shellsort(arr):
+    gap = len(arr)//2
+
+    while gap>0:
+        for i in range(gap, len(arr)):
+            anchor = arr[i]
+            j = i
+            while j>=gap and arr[j-gap] > anchor:
+                arr[j] = arr[j-gap]
+                j -= gap
+            arr[j] = anchor 
+        gap = gap//2
+
+
+elements = [89,78,61,53,23,21,17,12,9,7,6,2,1]
+shellsort(elements)
+print(elements)
